2d4b6e98981495478ee06ebf73e408a36193f332
Gijs Jorissen <gijs@360ecommerce.nl>
2014-06-10 15:51:26 +0200

    List-group closing tag

--- classes/class-wabbr-components.php
+++ classes/class-wabbr-components.php
@@ -16,6 +16,6 @@ class Wabbr_Components extends Wabbr_Shortcode
 		$content = str_replace( '<ul>', '<ul class="list-group">', $content );
 		$content = str_replace( '<li>', '<li class="list-group-item">', $content );
 
-		return '<div class="wabbr-list-group ' . $class . '">' . do_shortcode( $content ) . '</ul>';
+		return '<div class="wabbr-list-group ' . $class . '">' . do_shortcode( $content ) . '</div>';
 	}
 }
