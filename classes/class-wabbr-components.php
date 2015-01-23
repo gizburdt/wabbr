90c8206a8fc7efc3dc0b9cfdb8e42b47df676143
Gijs Jorissen <gijs@360ecommerce.nl>
2015-01-23 17:43:11 +0100

    Block

--- classes/class-wabbr-components.php
+++ classes/class-wabbr-components.php
@@ -4,7 +4,17 @@ class Wabbr_Components extends Wabbr_Shortcode
 {
 	function add_shortcodes()
 	{
+		add_shortcode( 'block', 		array( &$this, 'block' ) );
 		add_shortcode( 'list-group', 	array( &$this, 'list_group' ) );
+	}
+
+	function block( $atts, $content = null )
+	{
+		extract( shortcode_atts( array(
+			'class' 	=> '',
+		), $atts ) );
+
+		return '<div class="wabbr block ' . $class . '">' . do_shortcode( $content ) . '</div>';
 	}
 
 	function list_group( $atts, $content = null )
